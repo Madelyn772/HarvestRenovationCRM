62a65974679f8d417ac024a6a756080d5b780460
Madelyn Chesson <madelyn@Madelyns-MacBook-Air.local>
2026-05-22 18:10:06 -0500

    Added consistent Supabase key config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export const portalConfig = {
-  supabaseUrl: 'https://sctlkvmqxgkuwotdkduc.supabase.co/rest/v1/',
+  supabaseUrl: 'https://sctlkvmqxgkuwotdkduc.supabase.co',
   supabasePublishableKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNjdGxrdm1xeGdrdXdvdGRrZHVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk0MjAyOTQsImV4cCI6MjA5NDk5NjI5NH0.1UWfUO_4YfCJFkHCkF3SpBptF0FVH8sbXgh3KqSiwek',
   companyCalendarName: 'Harvest Renovation Company Calendar',
   companyCalendarEmbedUrl: 'https://calendar.google.com/calendar/embed?src=harvestrenovation%40gmail.com&ctz=America%2FChicago',
